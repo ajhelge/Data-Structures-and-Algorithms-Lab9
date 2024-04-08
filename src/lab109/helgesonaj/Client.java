0c94ad55a0c51a9cb2f2a8a2412713843f889336
Ahelge <111927502+Ahelge@users.noreply.github.com>
2024-04-08 14:22:02 -0500

    Initialize

---
+++ src/lab109/helgesonaj/Client.java
@@ -0,0 +1,16 @@
+package lab109.helgesonaj;
+
+/**
+ *
+ * @author ajhel
+ */
+public class Client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
